ae4d953c3384952d7dd1ebaef271f9aff280a71c
StpehenChinag <stephenchinag@gmail.com>
2023-11-15 17:48:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<script>
+const app = {
+  data() {
+    return {
+      friends: [
+        {
+          id: "stephen",
+          name: "Stephen Chinag",
+          phone: "0918938308",
+          email: "stephenChinag@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
